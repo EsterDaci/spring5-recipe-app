9ae2bd9732d191f56476a769c940da2503ebafc1
Ester Daci <edaci@dalet.com>
2023-09-30 21:25:58 +0200

    Adding Guacamole Recipe and Recipe Service.

--- src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
@@ -1,13 +1,38 @@
 package guru.springframework.spring5recipeapp.controllers;
 
+import guru.springframework.spring5recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+//    private CategoryRepository categoryRepository;
+//    private UnitOfMeasureRepository unitOfMeasureRepository;
+//
+//    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+//        this.categoryRepository = categoryRepository;
+//        this.unitOfMeasureRepository = unitOfMeasureRepository;
+//    }
+
+    private final RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
 
     @RequestMapping({"","/","/index"})
-    public String getIndexPage(){
+    public String getIndexPage(Model model){
+//        Optional<Category> category=categoryRepository.findByDescription("American");
+//        Optional<UnitOfMeasure> unitOfMeasure=unitOfMeasureRepository.findByDescription("Teaspoon");
+//
+//        System.out.println("Cat ID is:" + category.get().getId());
+//        System.out.println("UOM ID is:" + unitOfMeasure.get().getId());
+
+        model.addAttribute("recipes", recipeService.getRecipes());
+
+
         return "index";
     }
 }
--- src/main/java/guru/springframework/spring5recipeapp/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/spring5recipeapp/repositories/CategoryRepository.java
@@ -4,5 +4,9 @@ import guru.springframework.spring5recipeapp.Domain.Category;
 import io.micrometer.observation.Observation;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category,Long> {
+
+    Optional<Category> findByDescription(String description);
 }
