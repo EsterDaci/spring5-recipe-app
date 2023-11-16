b020bcdc0b676242f9f7bce223fd9c5fb929b40e
Ester Daci <edaci@dalet.com>
2023-11-16 09:45:53 +0100

    Make some small changes in Recipe Controller.

--- src/main/java/guru/springframework/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/RecipeController.java
@@ -9,23 +9,24 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RecipeController {
-    private final RecipeService recipeService;
 
+    private final RecipeService recipeService;
 
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
 
     @RequestMapping("/recipe/show/{id}")
-    public String showById(@PathVariable String id, Model model) {
-        model.addAttribute("recipe", recipeService.findById(Long.parseLong(id)));
+    public String showById(@PathVariable String id, Model model){
 
+        model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
-
     }
+
     @RequestMapping("recipe/new")
     public String newRecipe(Model model){
-        model.addAttribute("recipe",new RecipeCommand());
+        model.addAttribute("recipe", new RecipeCommand());
+
         return "recipe/recipeform";
     }
 
